93defccf0c663facf3c9cdeaa200fb5e5fc6a757
Abdulrahman Abotair <ab.hamdy@diva-lab.com>
2020-05-10 06:21:01 +0200

    all fields and latest cards sections => home page

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,2 +1,23 @@
 /*@abdulrahmanAbutair_mainJS*/
 ////////////////////////////////////////
+
+/*****************************************************
+* All field section => home page (show and hide fields)
+* */
+let fieldsHide = $('.fields>.row>[class^="col-"]:not(:nth-child(-n+8))'),
+    fieldsBtn = $('.fields__btn'),
+    fieldsBtnSpan = $('.fields__btn span');
+
+fieldsBtn.on('click', function () {
+    fieldsHide.toggleClass('fields__visible');
+    if (fieldsHide.hasClass('fields__visible')) {
+        fieldsBtnSpan.text('less')
+    } else {
+        fieldsBtnSpan.text('more')
+    }
+});
+/****************************************/
+
+
+
+

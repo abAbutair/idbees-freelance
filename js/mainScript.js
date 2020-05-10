6a2773075358795201b624e864a717fc66a18a72
Abdulrahman Abotair <ab.hamdy@diva-lab.com>
2020-05-10 08:04:35 +0200

    latest products section => home page

--- js/mainScript.js
+++ js/mainScript.js
@@ -2,20 +2,26 @@
 ////////////////////////////////////////
 
 /*****************************************************
-* All field section => home page (show and hide fields)
+* (show / hide fields) on grid system (rows/cols)
 * */
-let fieldsHide = $('.fields>.row>[class^="col-"]:not(:nth-child(-n+8))'),
-    fieldsBtn = $('.fields__btn'),
-    fieldsBtnSpan = $('.fields__btn span');
+(function () {
+    let fieldsBtn = $('.grid-to-hide__btn');
+    fieldsBtn.click( function () {
+        let fieldsHide = $(this).closest('.grid-to-hide').children('.row').children('[class^="col-"]:not(:nth-child(-n+8))'),
+            fieldsHideHeight = $(this).closest('.grid-to-hide').children('.row').children('[class^="col-"]').outerHeight(),
+            fieldsBtnSpan = $(this).children('span');
 
-fieldsBtn.on('click', function () {
-    fieldsHide.toggleClass('fields__visible');
-    if (fieldsHide.hasClass('fields__visible')) {
-        fieldsBtnSpan.text('less')
-    } else {
-        fieldsBtnSpan.text('more')
-    }
-});
+        fieldsHide.toggleClass('grid-to-hide__visible');
+
+        if (fieldsHide.hasClass('grid-to-hide__visible')) {
+            fieldsBtnSpan.text('less');
+            fieldsHide.css({'height': fieldsHideHeight + 'px'});
+        } else {
+            fieldsBtnSpan.text('more');
+            fieldsHide.css({'height': '0'});
+        }
+    });
+})();
 /****************************************/
 
 

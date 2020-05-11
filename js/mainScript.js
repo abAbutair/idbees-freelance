f7d474ce8ede1c5845674539ae539dd06aebf2de
Abdulrahman Abotair <ab.hamdy@diva-lab.com>
2020-05-12 01:12:07 +0200

    edit show more script, last 2 sections, sticky header, back to top button

--- js/mainScript.js
+++ js/mainScript.js
@@ -7,23 +7,102 @@
 (function () {
     let fieldsBtn = $('.grid-to-hide__btn');
     fieldsBtn.click( function () {
-        let fieldsHide = $(this).closest('.grid-to-hide').children('.row').children('[class^="col-"]:not(:nth-child(-n+8))'),
+
+        let fieldsHideWrapper = $(this).closest('.grid-to-hide'),
+            fieldsHideEven = $(this).closest('.grid-to-hide--even').children('.row').children('[class^="col-"]:not(:nth-child(-n+8))'),
+            fieldsHideOdd = $(this).closest('.grid-to-hide--odd').children('.row').children('[class^="col-"]:not(:nth-child(-n+9))'),
             fieldsHideHeight = $(this).closest('.grid-to-hide').children('.row').children('[class^="col-"]').outerHeight(),
             fieldsBtnSpan = $(this).children('span');
 
-        fieldsHide.toggleClass('grid-to-hide__visible');
+        fieldsHideWrapper.toggleClass('grid-to-hide__visible');
 
-        if (fieldsHide.hasClass('grid-to-hide__visible')) {
+        if (fieldsHideWrapper.hasClass('grid-to-hide__visible')) {
             fieldsBtnSpan.text('less');
-            fieldsHide.css({'height': fieldsHideHeight + 'px'});
+            fieldsHideEven.css({'height': fieldsHideHeight + 'px'});
+            fieldsHideOdd.css({'height': fieldsHideHeight + 'px'});
         } else {
             fieldsBtnSpan.text('more');
-            fieldsHide.css({'height': '0'});
+            fieldsHideEven.css({'height': '0'});
+            fieldsHideOdd.css({'height': '0'});
         }
     });
 })();
 /****************************************/
 
+/*****************************************************
+ * menu swipe open
+ * */
+$(function () {
+    // let tab_window = window.matchMedia("(max-width: 992px)");
+    // if (tab_window.matches) {
+    //
+    // }
+    $(".carousel").swipe({
+        //Generic swipe handler for all directions
+        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+            if (direction == 'left') $(this).carousel('next');
+            if (direction == 'right') $(this).carousel('prev');
+        },
+        allowPageScroll: "vertical",
+        threshold: 10,
+    });
+});
+/****************************************/
+
+/*****************************************************
+ * menu swipe open
+ * */
+(function () {
+    let favouriteBtn = $('.favourite');
+    favouriteBtn.click( function () {
+        $(this).toggleClass('favourite--added');
+    });
+})();
+/****************************************/
+
+/*****************************************************
+ * sticky nav script
+ * */
+(function () {
+    let header = document.querySelector("header");
+    let headerHeight = header.offsetHeight;
 
 
+    let main = document.querySelector("main");
+    main.style.paddingTop = 0 + 'px';
 
+
+    window.addEventListener('scroll', function () {
+        let windowPos = window.scrollY > 0;
+        if (windowPos) {
+            header.classList.add('fixed');
+            main.style.paddingTop = headerHeight + 'px';
+
+        } else {
+            header.classList.remove('fixed');
+            main.style.paddingTop = 0 + 'px';
+        }
+    });
+})();
+/****************************************/
+
+/*****************************************************
+ * Back to top button
+ * */
+(function () {
+    let back_to_top_btn = $('.back-to-top .btn');
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            back_to_top_btn.addClass('show');
+        } else {
+            back_to_top_btn.removeClass('show');
+        }
+    });
+
+    back_to_top_btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+    });
+
+})();
